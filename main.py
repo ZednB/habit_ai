a76939543aa89ae5ec3da0a530ff2826c27fbfe9
Zubayr Bibulatov <Ru.Zubayr@gmail.com>
2025-09-15 20:24:50 +0300

    Исправил мелочи

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 import uvicorn
 from fastapi import FastAPI
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 from users.routes import router
+from users.web_routes import router as web_router
 
 app = FastAPI()
 
 app.include_router(router)
+app.include_router(web_router)
+
+templates = Jinja2Templates(directory='templates')
+
+app.mount('/static', StaticFiles(directory='static'), name='static')
 
 if __name__ == '__main__':
     uvicorn.run('main:app', host='127.0.0.1', port=8000, reload=True)
